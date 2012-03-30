1da646bc5a30022b3477370bd17ec998887fce0e
Xiaowen Zhang <xiaowen1106@gmail.com>
2012-03-30 21:14:52 +0200

    share buttons and category images

--- acceuilNonConnecter.php
+++ acceuilNonConnecter.php
@@ -35,20 +35,46 @@
             <div id="left_container">
                 <ul id="cataglogue_list">
                     <li id="catalogue_recherche">recherche catalogue</li>
-                    <li id="catalogue_title">Catalogue</li>
-                    <li>Button logo Musique</li>
-                    <li>Button logo Photo</li>
-                    <li>Button logo Art</li>
-                    <li>Button logo Création</li>
-                    <li>Button logo Comédie</li>
+                    <li id="catalogue_title"><h2>Catégories</h2></li>
+                    <li><a href=""><img id="category_button" src="img/category1.jpg"></a></li>
+                    <li><a href=""><img id="category_button" src="img/category2.jpg"></a></li>
+                    <li><a href=""><img id="category_button" src="img/category3.jpeg"></a></li>
+                    <li><a href=""><img id="category_button" src="img/category4.jpg"></a></li>
+                    <li><a href=""><img id="category_button" src="img/category5.jpg"></a></li>
                 </ul>
             </div>
             <div id="center_container">
-                <div id="slogan_container">
-                    
-                </div>
-                <div id="share_container">
-                    
+                <div id="center_upper">
+                    <div id="slogan_container">
+                        <h1>WE ARE THE TALENTS</h1>
+                    </div>
+                    <div id="share_container">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td><a name="fb_share" type="button" share_url="YOUR_URL"></a>
+                                        <script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share"
+                                                type="text/javascript">
+                                        </script>
+                                    </td>
+                                    <td>
+                                        <script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+                                        <script type="IN/Share"></script>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <a href="https://twitter.com/share" class="twitter-share-button" data-lang="en" data-count="none">Tweet</a>
+                                        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+                                    </td>
+                                    <td>
+                                        <script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+                            <g:plusone annotation="none" size="tall" width="100px"></g:plusone>
+                            </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
                 <div id="talents">
                     <h2>Talent du mois</h2>
